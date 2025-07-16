52966735a36ac6ab406ebabf703d111d090d0e84
headonpro <cirakoglu.onur@gmail.com>
2025-07-17 00:44:04 +0200

    Meilenstein: Entferne deprecated images.domains aus next.config.js, Projekt jetzt komplett warnungsfrei und Next.js-konform

--- viktoria-wertheim-frontend/next.config.js
+++ viktoria-wertheim-frontend/next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['localhost', 'strapi.viktoria-wertheim.de', 'reliable-morning-f369ca7d6e.strapiapp.com'],
     remotePatterns: [
       {
         protocol: 'http',
